f8af7e2129f8e46ebb51af8b012b01bd025ca65e
Ayo Makanjuola <makville@gmail.com>
2020-01-03 14:26:00 +0100

    Added flexibility to path routing

--- config/routes.php
+++ config/routes.php
@@ -2,8 +2,9 @@
 use Cake\Routing\RouteBuilder;
 use Cake\Routing\Router;
 use Cake\Routing\Route\DashedRoute;
+use Cake\Core\Configure;
 
-Router::plugin('MakvilleAcl', ['path' => '/makville-acl'],
+Router::plugin('MakvilleAcl', ['path' => Configure::read('makville-acl-path', '/makville-acl')],
     function (RouteBuilder $routes) {
         $routes->fallbacks(DashedRoute::class);
     }
